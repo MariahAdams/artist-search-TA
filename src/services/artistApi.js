f5aeee081257de30499fd4434cb4a2209a54fb10
MariahAdams <mariah.adams23@yahoo.com>
2019-02-23 13:01:39 -0800

    updating api with getWorks function

--- src/services/artistApi.js
+++ src/services/artistApi.js
@@ -2,3 +2,8 @@ export const getArtists = async(query) => {
   const res = await fetch(`http://musicbrainz.org/ws/2/artist?query=${query}&fmt=json&limit=25`);
   return res.json();    
 };
+
+export const getWorks = async(artistId) => {
+  const res = await fetch(`http://musicbrainz.org/ws/2/artist/${artistId}?fmt=json&inc=works`);
+  return res.json();
+};
