fd83ba536d81c5961bec5e70f650eea25bd2489a
MariahAdams <mariah.adams23@yahoo.com>
2019-02-24 16:21:22 -0800

    updating api, attempting to deploy

--- src/services/artistApi.js
+++ src/services/artistApi.js
@@ -1,14 +1,20 @@
-export const getArtists = async(query) => {
-  const res = await fetch(`http://musicbrainz.org/ws/2/artist?query=${query}&fmt=json&limit=25`);
+const getArtists = async(query) => {
+  const res = await fetch(`https://musicbrainz.org/ws/2/artist?query=${query}&fmt=json&limit=25`);
   return res.json();    
 };
 
-export const getWorks = async(artistId) => {
-  const res = await fetch(`http://musicbrainz.org/ws/2/artist/${artistId}?fmt=json&inc=works`);
+const getWorks = async(artistId) => {
+  const res = await fetch(`https://musicbrainz.org/ws/2/artist/${artistId}?fmt=json&inc=works`);
   return res.json();
 };
 
-export const getLyrics = async(artist, song) => {
+const getLyrics = async(artist, song) => {
   const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
   return res.json();
+};
+
+export {
+  getArtists,
+  getWorks,
+  getLyrics
 };
